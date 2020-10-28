4e3cdb69dfe8c2291effc7b9d44cf7ac2849e198
M1044402 <M1044402@MT-PF10TTST>
2020-10-28 17:08:59 +0530

    MOSIP-9838 : corrected visible

--- registration/registration-services/src/main/java/io/mosip/registration/dto/schema/Group.java
+++ registration/registration-services/src/main/java/io/mosip/registration/dto/schema/Group.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.mosip.registration.dto.Validator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,7 +22,7 @@ public class Group {
 	@JsonProperty("css-class")
 	private String cssClass;
 	
-	private String visible;
+	private Validator visible;
 	private boolean enabled;
 	private int order;
 	private List<Field> fields;
