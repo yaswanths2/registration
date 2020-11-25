8fd94bd236b12213cfd65b312a0d16f6c4b6b634
HimajaDhanyamraju2 <dhanyamraju.himaja2@mindtree.com>
2020-11-25 19:10:16 +0530

    MOSIP-10268: Added DTO classes

---
+++ registration/registration-services/src/main/java/io/mosip/registration/dto/schema/Group.java
@@ -0,0 +1,31 @@
+package io.mosip.registration.dto.schema;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.mosip.registration.dto.UiSchemaDTO;
+import io.mosip.registration.dto.Validator;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Group {
+	
+	private String name;
+	private String layout;
+	
+	@JsonProperty("css-class")
+	private String cssClass;
+	
+	private Validator visible;
+	private boolean enabled;
+	private int order;
+	private List<UiSchemaDTO> fields;
+
+}
